6a923cb92ff2ab78f79d138aec2a2d4416598965
Preethi <preethi5014@gmail.com>
2024-03-19 14:58:37 +0800

    Expense V2

---
+++ expense-v2/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "frontend" {
+  for_each      = var.instance_types
+  ami           = var.ami
+  instance_type = each.value["instance_types"]
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_groups" {
+  default = ["sg-039ea9c77ec0fdcbd"]
+}
+
+variable "instance_types" {
+  default = {
+
+    frontend = {
+      instance_type = "t3.micro"
+    }
+
+    mysql = {
+      instance_type = "t3.small"
+    }
+
+    backend = {
+      instance_type = " t3.micro"
+    }
+  }
+}
